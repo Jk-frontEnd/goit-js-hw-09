336a272cf03979ab091b07a4d7d09fe587be866f
Jk-frontEnd <151435676+Jk-frontEnd@users.noreply.github.com>
2024-01-03 12:48:28 +0000

    Deploying to gh-pages from @ Jk-frontEnd/goit-js-hw-09@5928cee043bd73161ed0c9a3fe67549b7e8bc074 🚀

---
+++ 01-color-switcher.7ac672c8.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("start"),t=document.getElementById("stop"),d=document.querySelector("body");let l=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,l=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(l)}));
+//# sourceMappingURL=01-color-switcher.7ac672c8.js.map
